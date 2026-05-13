c7c79873acb8092b9bd681d377e0a04a070e8ccd
spwoodcock <sam.woodcock@protonmail.com>
2026-05-13 22:25:10 +0100

    fix(frontend): correctly use hotosm/ui in line with other apps

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,5 @@
+import "@hotosm/ui/dist/style-core.css";
+import "@hotosm/ui/dist/components/header/header.js";
 import maplibregl from "maplibre-gl";
 import { Protocol } from "pmtiles";
 import { StrictMode } from "react";
