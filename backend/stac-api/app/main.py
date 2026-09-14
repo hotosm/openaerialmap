d242017b873a78e9bda6992867c1a7bb6dc67a7a
Komal ray <komalray.ray11@gmail.com>
2026-09-14 12:05:17 -0400

    feat(stac): add optional Prometheus metrics export
    Signed-off-by: Komal ray <komalray.ray11@gmail.com>

--- backend/stac-api/app/main.py
+++ backend/stac-api/app/main.py
@@ -10,12 +10,14 @@ NOTE the API docs can be found here: https://api.imagery.hotosm.org/stac/api.htm
 import os
 from contextlib import asynccontextmanager
 
-from app.settings import Settings
+from app.settings import MonitoringTypes, Settings
 from brotli_asgi import BrotliMiddleware
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 from stac_fastapi.api.app import StacApi
-from stac_fastapi.api.middleware import CORSMiddleware, ProxyHeaderMiddleware
+#from stac_fastapi.api.middleware import CORSMiddleware, ProxyHeaderMiddleware
+from starlette.middleware.cors import CORSMiddleware
+from stac_fastapi.api.middleware import ProxyHeaderMiddleware
 from stac_fastapi.api.models import (
     EmptyRequest,
     ItemCollectionUri,
@@ -47,6 +49,7 @@ from stac_fastapi.pgstac.extensions.filter import FiltersClient
 from stac_fastapi.pgstac.transactions import BulkTransactionsClient, TransactionsClient
 from stac_fastapi.pgstac.types.search import PgstacSearch
 from starlette.middleware import Middleware
+from prometheus_fastapi_instrumentator import Instrumentator
 
 settings = Settings()
 
@@ -223,11 +226,13 @@ api = StacApi(
             CORSMiddleware,
             allow_origins=settings.cors_origins,
             allow_methods=settings.cors_methods,
+            expose_headers=[],
         ),
     ],
 )
 app = api.app
-
+if settings.monitoring == MonitoringTypes.PROMETHEUS:
+    Instrumentator().instrument(app).expose(app)
 
 def run():
     """Run app from command line using uvicorn if available."""
--- backend/stac-api/app/settings.py
+++ backend/stac-api/app/settings.py
@@ -1,5 +1,10 @@
 from stac_fastapi.pgstac.config import Settings as _Settings
+from enum import Enum
 
+class MonitoringTypes(str, Enum):
+    NONE = ""
+    PROMETHEUS = "prometheus"
 
 class Settings(_Settings):
     """Settings specific to this deployment of STAC FastAPI PgSTAC"""
+    monitoring: MonitoringTypes = MonitoringTypes.NONE
